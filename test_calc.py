3b212b743b6bc0ec20e6e44bf5579cf5f3479e03
C. E. Sharp <chrisesharp@gmail.com>
2018-12-03 16:10:22 +0000

    calculate repeating frequency

--- test_calc.py
+++ test_calc.py
@@ -1,13 +1,34 @@
-from calc import answer
+from calc import Calc
 
 def test_1():
+    calc = Calc()
     file = open("test1.txt", "r")
-    assert answer(file) == 3
+    assert calc.answer(file) == 3
     
 def test_2():
+    calc = Calc()
     file = open("test2.txt", "r")
-    assert answer(file) == 0
+    assert calc.answer(file) == 0
     
 def test_3():
+    calc = Calc()
     file = open("test3.txt", "r")
-    assert answer(file) == -6
+    assert calc.answer(file) == -6
+    
+def test_4():
+    calc = Calc()
+    file = open("test4.txt", "r")
+    assert calc.answer(file) == 0
+    assert calc.repeating_freq() == 0
+
+def test_5():
+    calc = Calc()
+    file = open("test5.txt", "r")
+    assert calc.answer(file) == 4
+    assert calc.repeating_freq() == 10
+    
+def test_6():
+    calc = Calc()
+    file = open("test6.txt", "r")
+    assert calc.answer(file) == 4
+    assert calc.repeating_freq() == 5
